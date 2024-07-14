a85ff30615b32ef48695909d855fb294f3d7ce39
Changmin Yi <ycm93dg@gmail.com>
2024-07-14 22:00:49 +0900

    add elice day 4 answer - checked, dfs

---
+++ elice2407/day4/ans4.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+import pdb
+
+v=defaultdict(list)
+dp=[None]*100050
+inf=int(1e12)
+
+def dfs(x:int ,par:int):
+    global dp,v
+    
+    ret=inf
+    
+    for nxt in v[x]:
+        if nxt==par:
+            continue
+            
+        dfs(nxt,x)
+        ret=min(ret,dp[nxt])
+        
+    if ret==inf:
+        ret=0
+        
+    dp[x]=x-ret
+    print("dp[",x,"]=",dp[x])
+    pdb.set_trace()
+
+n=int(input())
+
+for _ in range(1,n):
+    a,b=(map(int,input().split()))
+    v[a].append(b)
+    v[b].append(a)
+
+pdb.set_trace()
+dfs(1, 0)
+
+for i in range(1,n+1):
+    print('1' if dp[i]>=0 else '0')
