4318c7ab07d22bcdd04ed6ed14f36e3f47617741
Changmin Yi <ycm93dg@gmail.com>
2024-10-11 18:02:31 +0900

    [feat] add leetcode dirs

---
+++ leetcode/1942/1942.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        
+        
+        
+if __name__ == "__main__":
+    # 1
+    times = [[1,4],[2,3],[4,6]]
+    targetFriend = 1
+    
+    # 2
+    # times = [[3,10],[1,5],[2,6]]
+    # targetFriend = 0
+    
+    print(Solution().smallestChair(times, targetFriend))
